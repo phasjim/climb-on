e2dbe3415a76611663d1e5ca58ee5a3d34af2b47
Hasjim <phasjim@ussd-ofcmc4069.local>
2015-10-23 19:18:01 -0700

    Working scene. But the flowers aren't animating yet

--- directives/directives.js
+++ directives/directives.js
@@ -1,15 +1,47 @@
 (function(angular) {
-  'use strict';
-angular.module('portfolio', [])
-  /* --------------------- CONTROLLER --------------------- */
-  .controller('Controller', ['$scope', function($scope) {
+	'use strict';
+	angular.module('portfolio', [])
+		/* --------------------- CONTROLLER --------------------- */
+		.controller('Controller', ['$scope', function($scope) {
+		}])
 
-  }])
+		/* --------------------- DIRECTIVES --------------------- */
+		.directive('sunSvg', function() {
+			return {
+				templateUrl: 'directives/templates/sun-svg-template.html'
+			};
+		})
 
-  /* --------------------- DIRECTIVES --------------------- */
-  .directive('sunSvg', function() {
-    return {
-      templateUrl: 'directives/templates/sun-svg-template.html'
-    };
-  });
+		.directive('cliffSvg', function() {
+			return {
+				templateUrl: 'directives/templates/cliff-svg-template.html'
+			};
+		})
+
+		.directive('sandSvg', function() {
+			return {
+				templateUrl: 'directives/templates/sand-svg-template.html'
+			};
+		})
+
+		.directive('mountainsSvg', function() {
+			return {
+				templateUrl: 'directives/templates/mountains-svg-template.html'
+			};
+		})
+
+		.directive('cactiSvg', function() {
+			return {
+				templateUrl: 'directives/templates/cacti-svg-template.html'
+			};
+		})
+
+		.directive('cactiFlowerLightSvg', function() {
+			return {
+				templateUrl: 'directives/templates/cacti-flower-light-svg-template.html',
+				scope: {
+					index: "="
+				}
+			};
+		})
 })(window.angular);
